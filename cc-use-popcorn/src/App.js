a52e81ab305c334be90928d74299e6e44e6356f3
natalieflcn <natalie.dflcn@gmail.com>
2025-06-10 14:31:11 -0400

    Implemented local storage API to persist data across reloads

--- cc-use-popcorn/src/App.js
+++ cc-use-popcorn/src/App.js
@@ -57,12 +57,17 @@ const KEY = "572d11a1";
 // STRUCTURAL COMPONENTS
 export default function App() {
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
+  // const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
   const tempQuery = "interstellar";
+
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -79,6 +84,13 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
