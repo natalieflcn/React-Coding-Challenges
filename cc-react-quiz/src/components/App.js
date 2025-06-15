d224639387d41d8e43a3ece78f87c21a8c5b6731
natalieflcn <natalie.dflcn@gmail.com>
2025-06-15 18:07:22 -0400

    Rendered first question and options component

--- cc-react-quiz/src/components/App.js
+++ cc-react-quiz/src/components/App.js
@@ -12,6 +12,7 @@ const initialState = {
 
   // loading, error, ready, active, finished
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -41,7 +42,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   const numQuestions = questions.length;
 
   useEffect(function () {
@@ -60,7 +64,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
