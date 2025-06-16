84604bd251b3ff41bbab6fcfcfa0e620405e1086
natalieflcn <natalie.dflcn@gmail.com>
2025-06-16 02:57:30 -0400

    Polished formatting of timer

--- cc-react-quiz/src/components/Timer.js
+++ cc-react-quiz/src/components/Timer.js
@@ -17,7 +17,9 @@ function Timer({ dispatch, secondsRemaining }) {
 
   return (
     <div className="timer">
-      {mins}:{seconds}
+      {mins < 10 && "0"}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
     </div>
   );
 }
