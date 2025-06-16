bbbe05e5542c8e306b36701ab5c2ce06c603b986
natalieflcn <natalie.dflcn@gmail.com>
2025-06-15 20:02:09 -0400

    Implemented functionality for rendering correct and incorrect answers

--- cc-react-quiz/src/components/App.js
+++ cc-react-quiz/src/components/App.js
@@ -13,6 +13,8 @@ const initialState = {
   // loading, error, ready, active, finished
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -36,13 +38,25 @@ function reducer(state, action) {
         status: "active",
       };
 
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -64,7 +78,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            answer={answer}
+            dispatch={dispatch}
+          />
+        )}
       </Main>
     </div>
   );
