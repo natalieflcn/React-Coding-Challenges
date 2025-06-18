ecb8bac99e36d048fc91e8db2a16bb9d96e20e13
natalieflcn <natalie.dflcn@gmail.com>
2025-06-18 03:19:36 -0400

    Implemented useNavigation hook to programmatically navigate to our Form component from the Map component

--- cc-worldwise/src/components/City.jsx
+++ cc-worldwise/src/components/City.jsx
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -18,6 +19,8 @@ function City() {
   };
 
   const { cityName, emoji, date, notes } = currentCity;
+
+  const { id } = useParams();
 
   return (
     <div className={styles.city}>
@@ -51,9 +54,7 @@ function City() {
         </a>
       </div>
 
-      <div>
-        <ButtonBack />
-      </div>
+      <div>{/* <ButtonBack /> */}</div>
     </div>
   );
 }
