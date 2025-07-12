956b675314ff59c9ebf1f099cd0b2e7d4478085b
natalieflcn <natalie.dflcn@gmail.com>
2025-07-12 16:02:37 -0400

    Creating complex selector hooks to manipulate UI state for the Cart

--- cc-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ cc-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,8 +1,22 @@
+import { useDispatch } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utilities/helpers";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -16,9 +30,13 @@ function MenuItem({ pizza }) {
         <p className="text-sm capitalize italic text-stone-500">
           {ingredients.join(", ")}
         </p>
-        <div className="flex mt-auto justify-between items-center">
+        <div className="mt-auto flex items-center justify-between">
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
