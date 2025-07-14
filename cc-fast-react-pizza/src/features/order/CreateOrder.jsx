cd5b6437e04c2aa13c67e863dad5105762748be4
natalieflcn <natalie.dflcn@gmail.com>
2025-07-13 23:31:40 -0400

    Used Thunks with RTK to asynchronously fetch user's geolocation and reverse geocode their coorindates to determine their location. Used this data to prepopulate the Address field, and submitted their coordinates with the order when the Order Form was submitted.

--- cc-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ cc-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart } from "../cart/cartSlice";
 import store from "../../store";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -42,7 +43,16 @@ function CreateOrder() {
   const formErrors = useActionData();
   const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
-  const username = useSelector((store) => store.user.username);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((store) => store.user);
+
+  const dispatch = useDispatch();
+  const isLoadingAddress = addressStatus === "loading";
 
   return (
     <div className="px-4 py-6">
@@ -72,16 +82,37 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               className="input w-full"
               type="text"
               name="address"
+              disabled={isLoadingAddress}
+              defaultValue={address}
               required
             />
+            {addressStatus === "error" && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>
+            )}
           </div>
+
+          {!position.latitude && !position.longitude && (
+            <span className="absolute right-[3px] top-[3px] z-50 md:right-[5px] md:top-[5px]">
+              <Button
+                type="small"
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+              >
+                Get position
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -100,7 +131,16 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type="primary">
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.longitude
+                ? `${position.longitude}, ${position.latitude}`
+                : ""
+            }
+          />
+          <Button disabled={isSubmitting || isLoadingAddress} type="primary">
             {isSubmitting ? "Placing Order..." : "Order now"}
           </Button>
         </div>
