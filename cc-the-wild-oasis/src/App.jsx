4359a6d5002b1b0ca236a6d3ccc1ddb7d78822eb
natalieflcn <natalie.dflcn@gmail.com>
2025-07-17 13:25:51 -0400

    Started building out navigation menu in the sidebar of the app

--- cc-the-wild-oasis/src/App.jsx
+++ cc-the-wild-oasis/src/App.jsx
@@ -1,9 +1,15 @@
 import GlobalStyles from "./styles/GlobalStyles";
-import Row from "./styles/Row";
-import Button from "./ui/Button";
-import Input from "./styles/Input";
-import styled from "styled-components";
-import { BrowserRouter, Navigate } from "react-router-dom";
+
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -11,15 +17,17 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="account" element={<Account />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="account" element={<Account />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+          </Route>
           <Route path="login" element={<Login />} />
-          <Route path="pageNotFound" element={<PageNotFound />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="users" element={<Users />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
