a49f4f43459966e221704045394471877dd4aa0b
natalieflcn <natalie.dflcn@gmail.com>
2025-07-21 15:40:20 -0400

    Added React Hook Form Library to project to help manage forms for cabins

--- cc-the-wild-oasis/src/App.jsx
+++ cc-the-wild-oasis/src/App.jsx
@@ -12,6 +12,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: { staleTime: 60 * 1000 },
@@ -37,6 +38,22 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: { duration: 3000 },
+          error: { duration: 5000 },
+          style: {
+            fontsize: "16px",
+            madWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
